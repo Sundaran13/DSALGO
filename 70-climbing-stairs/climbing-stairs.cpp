f4c0ce50775e5776a305d73d1df0d70675dc15db
Perceiver13 <63909437+Sundaran13@users.noreply.github.com>
2023-09-08 15:10:03 +0530

    Time: 0 ms (100.00%) | Memory: 6.1 MB (43.36%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -11,7 +11,11 @@ public:
         return dp[n]=fib(n-1)+ fib(n-2);
     }
     int climbStairs(int n) {
-
-        return fib(n);
+        dp[1]=1;
+        dp[0]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+ dp[i-2];
+        }
+        return dp[n];
     }
 };
