ca4ff79ef530afa9bbb848f6ebd5b7bc818f94d5
Perceiver13 <63909437+Sundaran13@users.noreply.github.com>
2023-09-09 14:12:04 +0530

    Time: 5 ms (12.25%) | Memory: 6.4 MB (5.82%) - LeetSync

--- 1086-divisor-game/divisor-game.cpp
+++ 1086-divisor-game/divisor-game.cpp
@@ -1,11 +1,19 @@
 class Solution {
 public:
-    bool divisorGame(int n) {
-        if(n%2==0){
-            return true;
-        }
-        else{
+    bool solver(int n,vector<int>&dp){
+        if(n==1){
             return false;
         }
+        if(dp[n]!=-1)return dp[n];
+        for(int i=1;i<n;i++){
+            if(n%i==0 and !solver(n-i,dp)){
+                return dp[n]=true;
+            }
+        }
+        return dp[n]=false;
+    }
+    bool divisorGame(int n) {
+        vector<int> dp(n+1,-1);
+        return solver(n,dp);
     }
 };
