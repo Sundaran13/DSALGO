54c97e77218ce6fd606f3024a27289203d19f031
Perceiver13 <63909437+Sundaran13@users.noreply.github.com>
2023-09-10 15:51:10 +0530

    Time: 63 ms (92.77%) | Memory: 9 MB (75.79%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -12,7 +12,18 @@ public:
         return dp[n]=ans;
     }
     int numSquares(int n) {
-        vector<int> dp(n+1,-1);
-        return solver(n,dp);
+        vector<int> dp(n+1);
+
+        for(int i=1;i<=n;i++){
+            if(i<4){
+                dp[i]=i;continue;
+            }
+            int ans=i;
+            for(int j=1;j*j<=i;j++){
+                ans=min(ans,1+dp[i-j*j]);
+            }
+            dp[i]=ans;
+            }
+        return dp[n];
     }
 };
