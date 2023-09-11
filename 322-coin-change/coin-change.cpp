5f4d212579eb41025dc873a30d32e57c0fa13a23
Perceiver13 <63909437+Sundaran13@users.noreply.github.com>
2023-09-11 12:41:34 +0530

    Time: 140 ms (18.00%) | Memory: 49.2 MB (17.45%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -18,8 +18,36 @@ public:
         return dp[curr][amount]=res;
     }
     int coinChange(vector<int>& coins, int amount) {
-        vector<vector<int>>dp(coins.size(),vector<int>(amount+1,-1));
-        int res=solver(coins,0,amount,dp);
+        vector<vector<int>>dp(coins.size()+1,vector<int>(amount+1,-1));
+        int Amount=amount;
+ 
+        for(int i=0;i<=amount;i++){
+            dp[coins.size()][i]=INT_MAX-1;
+        }
+        for(int i=0;i<coins.size()+1;i++){
+            dp[i][0]=0;
+        }
+        for(int curr=coins.size()-1;curr>=0;curr--){
+            for(int amount=0;amount<=Amount;amount++){
+                if(curr==coins.size() or amount==0){
+                    if(amount==0)dp[curr][amount]=0;
+                    else dp[curr][amount]=INT_MAX-1;
+                    continue;
+                }
+                int res=-1;
+                if(coins[curr]>amount){
+                    int dont_take=0+dp[curr+1][amount+0];
+                    res=dont_take;
+                }
+                else{
+                    int take=1+dp[curr+0][amount-coins[curr]];
+                    int dont_take=0+dp[curr+1][amount-0];
+                    res=min(take,dont_take);
+                }
+                dp[curr][amount]=res;
+                }
+            }
+        int res=dp[0][amount];
         return (res==INT_MAX -1)?-1:res;
     }
 };
