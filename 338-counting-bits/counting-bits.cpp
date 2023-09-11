1a9b4a3921848d5e0d31187b7bdca35e075f5d2e
Perceiver13 <63909437+Sundaran13@users.noreply.github.com>
2023-09-11 14:08:46 +0530

    Time: 3 ms (84.35%) | Memory: 8.7 MB (6.67%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
-    int solver(int n){
+    int solver(int n,vector<int>&dp){
         if(n==0){
             return 0;
         }
-        return n%2+solver(n/2);
+        if(dp[n]!=-1)return dp[n];
+        return dp[n]=n%2+solver(n/2,dp);
     }
     vector<int> countBits(int n) {
         vector<int> res(n+1);
-        for(int i=0;i<=n;i++){
-            res[i]=solver(i);
+        vector<long long int> dp(n+1,0);
+        for(long long int i=0;i<=n;i++){
+            dp[i]=i%2+dp[i/2];
+            res[i]=dp[i];            
         }
         return res;
     }
