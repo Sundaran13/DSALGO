cc88861cafed10060802bf9309737bed693a67e4
Perceiver13 <63909437+Sundaran13@users.noreply.github.com>
2023-09-12 12:44:45 +0530

    Time: 0 ms (100.00%) | Memory: 6.2 MB (35.07%) - LeetSync

--- 343-integer-break/integer-break.cpp
+++ 343-integer-break/integer-break.cpp
@@ -11,7 +11,21 @@ public:
         return dp[n]=maxi;
     }
     int integerBreak(int n) {
-        vector<int> dp(n+1,-1);
-        return solver(n,dp);
+        if(n==2){
+            return 1;
+        }
+        if(n==3){
+            return 2;
+        }
+        vector<int> dp(n+1,0);
+        dp[2]=1;dp[3]=2;
+        for(int i=2;i<=n;i++){
+            int maxi=0;
+            for(int j=1;j<i;j++){
+                maxi=max({maxi,j*dp[i-j],(i-j)*j});
+            }
+            dp[i]=maxi;
+            }
+        return dp[n];
     }
 };
