3acfd1171ba1bd5061d5103e1680349ac9156f6d
Perceiver13 <63909437+Sundaran13@users.noreply.github.com>
2023-09-12 15:36:46 +0530

    Time: 226 ms (8.73%) | Memory: 96.4 MB (8.48%) - LeetSync

--- 376-wiggle-subsequence/wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/wiggle-subsequence.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
-    int solver(vector<int>&nums,int curr,int prev,bool positive){
+    int solver(vector<int>&nums,int curr,int prev,bool positive,vector<vector<vector<int>>>&dp){
         if(curr>=nums.size() or prev>=nums.size()){
             return 0;
         }
+        if(dp[curr][prev][positive]!=-1)return dp[curr][prev][positive];
         if(positive){
             if(nums[prev]>nums[curr]){
-                return 1+solver(nums,curr+1,curr,!positive);
+                return dp[curr][prev][positive]=1+solver(nums,curr+1,curr,!positive,dp);
             }
             else{
-                return solver(nums,curr+1,curr,positive);
+                return dp[curr][prev][positive]=solver(nums,curr+1,curr,positive,dp);
             }
         }
         else{
             if(nums[prev]<nums[curr]){
-                return 1+solver(nums,curr+1,curr,!positive);
+                return dp[curr][prev][positive]=1+solver(nums,curr+1,curr,!positive,dp);
             }
             else{
-                return solver(nums,curr+1,curr,positive);
+                return dp[curr][prev][positive]=solver(nums,curr+1,curr,positive,dp);
             }
         }
     }
     int wiggleMaxLength(vector<int>& nums) {
-        return 1+max(solver(nums,1,0,true),solver(nums,1,0,false));
+        vector<vector<vector<int>>> dp(nums.size(),vector<vector<int>>(nums.size(),vector<int>(2,-1)));
+        return 1+max(solver(nums,1,0,true,dp),solver(nums,1,0,false,dp));
     }
 };
