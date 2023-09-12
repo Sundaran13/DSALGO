b97c59e991b9d4893056cc56e1cd3f6cf53e2761
Perceiver13 <63909437+Sundaran13@users.noreply.github.com>
2023-09-12 14:05:53 +0530

    Time: 0 ms (100.00%) | Memory: 6.1 MB (25.07%) - LeetSync

--- 357-count-numbers-with-unique-digits/count-numbers-with-unique-digits.cpp
+++ 357-count-numbers-with-unique-digits/count-numbers-with-unique-digits.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
-    int solver(int n){
+    int solver(int n,vector<int>&dp){
         if(n==0)return 1;
+        if(dp[n]!=-1)return dp[n];
         int sum=9;
         for(int i=0;i<n-1;i++){
             sum*=(9-i);
         }
-        return sum+solver(n-1);
+        return dp[n]=sum+solver(n-1,dp);
     }
     int countNumbersWithUniqueDigits(int n) {
-       return solver(n);
+        vector<int> dp(n+1,-1);
+       return solver(n,dp);
     }
 };
