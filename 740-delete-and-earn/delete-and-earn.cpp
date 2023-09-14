ec77c4b5ec92875e8421c2e7139e41a732428c2f
Perceiver13 <63909437+Sundaran13@users.noreply.github.com>
2023-09-14 15:25:38 +0530

    Time: 13 ms (52.91%) | Memory: 12.2 MB (77.61%) - LeetSync

--- 740-delete-and-earn/delete-and-earn.cpp
+++ 740-delete-and-earn/delete-and-earn.cpp
@@ -16,8 +16,21 @@ public:
         return dp[i]=max(curr_sum+solver(nums,index,dp),solver(nums,i+1,dp));
     }
     int deleteAndEarn(vector<int>& nums) {
-        vector<int> dp(nums.size(),-1);
+        vector<int> dp(nums.size()+1,0);
         sort(nums.begin(),nums.end());
-        return solver(nums,0,dp);
+        for(int i=nums.size()-1;i>=0;i--){
+            int curr_val=nums[i];
+            int curr_sum=nums[i];
+            int index=i+1;
+            while(index<nums.size() and nums[index]==curr_val){
+                curr_sum+=nums[index];
+                index++;
+            }
+            while(index<nums.size() and nums[index]==curr_val+1){
+                index++;
+            }
+            dp[i]=max(curr_sum+dp[index],dp[i+1]);
+            }
+        return dp[0];
     }
 };
