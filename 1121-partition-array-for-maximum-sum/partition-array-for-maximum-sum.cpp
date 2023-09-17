5ebcea0b7ed5b8a7c8b5371a0f6e6c942825310e
Perceiver13 <63909437+Sundaran13@users.noreply.github.com>
2023-09-17 14:19:30 +0530

    Time: 15 ms (60.90%) | Memory: 8.9 MB (8.67%) - LeetSync

---
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int solver(vector<int>&arr,int k,int s,vector<int>&dp){
+        if(s>=arr.size()){
+            return 0;
+        }
+        if(dp[s]!=-1)return dp[s];
+        int n=arr.size();
+        int curr_sum=INT_MIN;
+        int max_ele=INT_MIN;
+        for(int i=s;i<min(s+k,n);i++){
+            max_ele=max(max_ele,arr[i]);
+            curr_sum=max(curr_sum,solver(arr,k,i+1,dp)+max_ele*(i-s+1));
+        }
+        return dp[s]=curr_sum;
+    }
+    int maxSumAfterPartitioning(vector<int>& arr, int k) {
+        vector<int> dp(arr.size(),-1);
+        return solver(arr,k,0,dp);
+    }
+};
