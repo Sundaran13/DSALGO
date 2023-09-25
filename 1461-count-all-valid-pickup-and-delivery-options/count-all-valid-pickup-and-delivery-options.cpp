528a25f6f16866476081d4d9e557a654b3915103
Perceiver13 <63909437+Sundaran13@users.noreply.github.com>
2023-09-25 17:40:10 +0530

    Time: 3 ms (15.79%) | Memory: 7 MB (8.95%) - LeetSync

--- 1461-count-all-valid-pickup-and-delivery-options/count-all-valid-pickup-and-delivery-options.cpp
+++ 1461-count-all-valid-pickup-and-delivery-options/count-all-valid-pickup-and-delivery-options.cpp
@@ -1,7 +1,7 @@
 //#define MOD 1e9+7
 class Solution {
 public:
-    long long int MOD=1e9+7;
+    const int MOD=1e9+7;
     long long solver(long long n){
         if(n==1){
             return 1;
@@ -9,6 +9,11 @@ public:
         return ((2*n-1)*n*solver(n-1))%MOD;
     }
     int countOrders(int n) {
-       return solver(n); 
+        vector<long long int> dp(n+1);
+        dp[1]=1;
+       for(int i=2;i<=n;i++){
+           dp[i]=(dp[i-1]*(2*i-1)*i)%MOD;
+       }
+       return (int)dp[n]; 
     }
 };
