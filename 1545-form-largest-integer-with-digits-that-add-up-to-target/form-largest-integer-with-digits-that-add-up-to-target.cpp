9f7839474605afd4f618e2f7289f2d80c4e3ef46
Perceiver13 <63909437+Sundaran13@users.noreply.github.com>
2023-09-27 16:18:43 +0530

    Time: 466 ms (23.05%) | Memory: 722.5 MB (14.19%) - LeetSync

---
+++ 1545-form-largest-integer-with-digits-that-add-up-to-target/form-largest-integer-with-digits-that-add-up-to-target.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string solver(vector<int>&cost,int target,int idx,vector<vector<string>>&dp){
+        if(target==0)return "";
+        if(target<0 or idx>=cost.size())return "0";
+        if(dp[idx][target]!="-1")return dp[idx][target];
+        string include=to_string(idx+1)+solver(cost,target-cost[idx],0,dp);
+        string exclude=solver(cost,target,idx+1,dp);
+        return dp[idx][target]=getBigger(include,exclude);
+    }
+    string getBigger(string &s1,string &s2){
+        string t="0";
+        if(s1.find(t)!=std::string::npos)return s2;
+        if(s2.find(t)!=std::string::npos)return s1;
+        if(s1.length()>s2.length())return s1;
+        else return s2;
+    }
+    string largestNumber(vector<int>& cost, int target) {
+        int n=cost.size();
+        vector<vector<string>> dp(cost.size(),vector<string>(target+1,"-1"));
+        return solver(cost,target,0,dp);
+    }
+};
