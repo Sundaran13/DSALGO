d4d364bc1c375fe8b111d87fcde7468a1b516757
Perceiver13 <63909437+Sundaran13@users.noreply.github.com>
2023-10-02 01:14:24 +0530

    Time: 286 ms (36.20%) | Memory: 62.9 MB (8.00%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
+    map<pair<int,int>,int> dp;
     int solver(vector<int>&nums,int target,int index,int total){
         if(index>=nums.size()){
             if(total==target)return 1;
             else return 0;
         }
+        if(dp.find({index,total})!=dp.end())return dp[{index,total}];
         int positive=solver(nums,target,index+1,total+nums[index]);
         int negetive=solver(nums,target,index+1,total-nums[index]);
-        return positive+negetive;
+        return dp[{index,total}]=positive+negetive;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
+        //memset(dp,-1,sizeof(dp));
         return solver(nums,target,0,0);
     }
 };
