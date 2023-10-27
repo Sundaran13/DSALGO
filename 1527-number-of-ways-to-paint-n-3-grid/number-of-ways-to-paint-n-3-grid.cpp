5dff907547ea0070cab70ac14998e83f15820f4d
Perceiver13 <63909437+Sundaran13@users.noreply.github.com>
2023-10-27 13:47:12 +0530

    Time: 294 ms (14.04%) | Memory: 40.6 MB (13.28%) - LeetSync

---
+++ 1527-number-of-ways-to-paint-n-3-grid/number-of-ways-to-paint-n-3-grid.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int dp[5001][4][4][4]={};
+    int solver(int n,int a0,int b0,int c0){
+        if(n==0)return 1;
+        if(dp[n][a0][b0][c0]!=0)return dp[n][a0][b0][c0];
+        int ans=0;
+        vector<int> colors = {1,2,3};
+        for(int a:colors){
+            if(a==a0)continue;
+            for(int b:colors){
+                if(b==b0 or b==a)continue;
+                for(int c:colors){
+                    if(c==c0 or c==b)continue;
+                    ans+=solver(n-1,a,b,c);
+                    ans%=1000000007;
+                }
+            }
+        }
+        return dp[n][a0][b0][c0]=ans;
+    }
+    int numOfWays(int n) {
+        return solver(n,0,0,0);
+    }
+};
