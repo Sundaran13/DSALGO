be01e1ab6d7e07e7107cf69e8ec9dffaba4f45cd
Perceiver13 <63909437+Sundaran13@users.noreply.github.com>
2023-10-28 21:02:09 +0530

    Time: 660 ms (5.63%) | Memory: 415.3 MB (9.98%) - LeetSync

---
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int dp[1001][1001];
+    bool solver(string s,int i,int j){
+        if(i==j){
+            return true;
+        }
+        if(s[i]!=s[j]){
+            return false;
+        }
+        if(dp[i][j]!=-1)return dp[i][j];
+        if(i<j+1)
+            return dp[i][j]=solver(s,i+1,j-1);
+        return dp[i][j]=true;
+    }
+    int countSubstrings(string s) {
+        memset(dp,-1,sizeof(dp));
+        int ans=0;
+        for(int i=0;i<s.size();i++){
+            for(int j=s.size()-1;j>=i;j--){
+                ans+=solver(s,i,j);
+            }
+        }
+        return ans;
+    }
+};
