ce8a1798cf6d8d3b70eca3192b3075e138e586d6
Perceiver13 <63909437+Sundaran13@users.noreply.github.com>
2023-10-28 17:43:35 +0530

    Time: 4 ms (44.94%) | Memory: 7.7 MB (16.09%) - LeetSync

--- 413-arithmetic-slices/arithmetic-slices.cpp
+++ 413-arithmetic-slices/arithmetic-slices.cpp
@@ -2,10 +2,12 @@ class Solution {
 public:
     map<pair<int,int>,int> dp;
     int solver(vector<int>&nums,int i,int diff){
+        //Base case
         if(i==nums.size()-1 or nums[i+1]-nums[i]!=diff){
             return 0;
         }
         if(dp.find({i,diff})!=dp.end())return dp[{i,diff}];
+        //recursve call for the same difference...that is being carried on...
         return dp[{i,diff}]=1+solver(nums,i+1,diff);
     }
     int numberOfArithmeticSlices(vector<int>& nums) {
@@ -13,8 +15,11 @@ public:
             return 0;
         }
         int ans=0;
+        //Applying dp on intervals of the arrray..
         for(int i=0;i<nums.size()-2;i++){
+            //checking for every difference pair.
             int diff=nums[i+1]-nums[i];
+
             ans+=solver(nums,i+1,diff);
         }
         return ans;
