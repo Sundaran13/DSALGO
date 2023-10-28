07c6e2985942d70423fb3464b04dc3200a7381d6
Perceiver13 <63909437+Sundaran13@users.noreply.github.com>
2023-10-28 14:59:33 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (16.09%) - LeetSync

--- 413-arithmetic-slices/arithmetic-slices.cpp
+++ 413-arithmetic-slices/arithmetic-slices.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
+    map<pair<int,int>,int> dp;
     int solver(vector<int>&nums,int i,int diff){
         if(i==nums.size()-1 or nums[i+1]-nums[i]!=diff){
             return 0;
         }
-        return 1+solver(nums,i+1,diff);
+        if(dp.find({i,diff})!=dp.end())return dp[{i,diff}];
+        return dp[{i,diff}]=1+solver(nums,i+1,diff);
     }
     int numberOfArithmeticSlices(vector<int>& nums) {
         if(nums.size()<3){
