84051fe8d05449944c489fff4231d32e18d52550
Perceiver13 <63909437+Sundaran13@users.noreply.github.com>
2023-10-28 18:22:45 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (45.04%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
+    int dp[21][21][2];
     int solver(vector<int>&nums,int i,int j,bool player){
         if(i>j)return -1;
         if(i==j)return nums[i];
+        if(dp[i][j][player]!=-1)return dp[i][j][player];
         if(player){
-            return max(nums[i]+solver(nums,i+1,j,!player),nums[j]+solver(nums,i,j-1,!player));
+            return dp[i][j][player]=max(nums[i]+solver(nums,i+1,j,!player),nums[j]+solver(nums,i,j-1,!player));
         }
         else{
-            return min(-nums[i]+solver(nums,i+1,j,!player),-nums[j]+solver(nums,i,j-1,!player));
+            return dp[i][j][player]=min(-nums[i]+solver(nums,i+1,j,!player),-nums[j]+solver(nums,i,j-1,!player));
         }
     }
     bool predictTheWinner(vector<int>& nums) {
+        memset(dp,-1,sizeof dp);
         if(solver(nums,0,nums.size()-1,true)>=0){
             return true;
         }
