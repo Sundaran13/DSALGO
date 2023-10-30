2181fb2a0181ba9bb32e6eebf5bff35352bdb604
Perceiver13 <63909437+Sundaran13@users.noreply.github.com>
2023-10-30 21:50:17 +0530

    Time: 224 ms (87.92%) | Memory: 8.9 MB (86.86%) - LeetSync

--- 312-burst-balloons/burst-balloons.cpp
+++ 312-burst-balloons/burst-balloons.cpp
@@ -1,52 +1,28 @@
 class Solution {
 public:
-    int maxCoins(vector<int>& nums) {
-        // preprocessing: remove all zeros, insert 1 into the begin and the end
-        vector<int> cleaned = {1};
-        for (auto &num : nums)
-            if (num) cleaned.push_back(num);
-        cleaned.push_back(1);
-
-        int n = cleaned.size()-2;
-        // dp[start][end] means the max score we can get from bursting [start, end] balloons.
-        vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
-
-        return maxCoins(1, n, cleaned, dp);
+    int dp[301][301];
+    int solver(vector<int>&cleaned,int start,int end){
+        if(start>end){
+            return 0;
+        }
+        if(start==end){
+            return dp[start][end]=cleaned[start-1]*cleaned[start]*cleaned[start+1];
+        }
+        if(dp[start][end]!=-1)return dp[start][end];
+        int ans=INT_MIN;
+        for(int i=start;i<=end;i++){
+            ans=max(ans,cleaned[start-1]*cleaned[i]*cleaned[end+1]+solver(cleaned,start,i-1)+solver(cleaned,i+1,end));
+        }
+        return dp[start][end]=ans;
     }
-private:
-    // the real workhorse
-    int maxCoins(int start, int end, vector<int> &nums, vector<vector<int>> &dp) {
-        // empty
-        if (start>end) return 0;
-
-        // if the result is ready, return it directly.
-        if (dp[start][end]!=-1)
-            return dp[start][end];
-
-        // the boundary of recursion.
-        if (start == end) {
-            dp[start][end] = nums[start-1]*nums[start]*nums[start+1];
-            return dp[start][end];
+    int maxCoins(vector<int>& nums) {
+        vector<int> cleaned={1};
+        for(auto &it:nums){
+            if(it)cleaned.push_back(it);
         }
-
-        /**
-         *
-         * max score of [start, end] can be obtained as follow:
-         * let lastBurst point to the last balloon to be bursted,
-         * the score is the sum of:
-         *  1. left part to the lastBurst, i.e, [start, lastBurst-1]
-         *  2. the score of nums[start-1], nums[lastBurst], nums[end+1],
-         *     since all the balloons in [start, lastBurst-1] and [lastBurst+1, end] are bursted.
-         *  3. right part from the lastBurst, i.e, [lastBurst+1, end]
-         * Just iterate lastBurst from start to end to calculate the max score of [start, end]
-         *
-         */
-        for (int lastBurst=start; lastBurst<=end; ++lastBurst) {
-            dp[start][end] = max(dp[start][end],
-                                nums[start-1]*nums[lastBurst]*nums[end+1] +
-                                    maxCoins(start, lastBurst-1, nums, dp) +
-                                    maxCoins(lastBurst+1, end, nums, dp));
-        }
-        return dp[start][end];
+        memset(dp,-1,sizeof dp);
+        cleaned.push_back(1);
+        int n=cleaned.size()-2;
+        return solver(cleaned,1,n);
     }
 };
