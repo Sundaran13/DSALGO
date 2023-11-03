991407952ee1b4b4753ee5be3b08b8c2135e3944
Perceiver13 <63909437+Sundaran13@users.noreply.github.com>
2023-11-03 16:21:13 +0530

    Time: 992 ms (10.59%) | Memory: 246 MB (11.77%) - LeetSync

--- 1723-maximum-number-of-achievable-transfer-requests/maximum-number-of-achievable-transfer-requests.cpp
+++ 1723-maximum-number-of-achievable-transfer-requests/maximum-number-of-achievable-transfer-requests.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    int dp[17][100000];
     void solver(int n,vector<vector<int>>&requests,int i,int bitmask,int tot,int &ans){
         if(i==requests.size()){
             vector<int> nums(n,0);
@@ -19,11 +20,14 @@ public:
             ans=max(ans,tot);
             return;
         }
+        if(dp[i][bitmask]!=-1)return;
         solver(n,requests,i+1,bitmask,tot,ans);
         solver(n,requests,i+1,bitmask|(1<<i),tot+1,ans);
+        dp[i][bitmask]=1;
         return ;
     }
     int maximumRequests(int n, vector<vector<int>>& requests) {
+        memset(dp,-1,sizeof dp);
         int ans=INT_MIN;
         solver(n,requests,0,0,0,ans);
         return ans;
