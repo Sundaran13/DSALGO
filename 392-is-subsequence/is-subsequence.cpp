257faa99ec86e09cb90b24463c066ebef9379c8f
Perceiver13 <63909437+Sundaran13@users.noreply.github.com>
2023-11-06 12:26:22 +0530

    Time: 9 ms (5.16%) | Memory: 11.7 MB (5.09%) - LeetSync

--- 392-is-subsequence/is-subsequence.cpp
+++ 392-is-subsequence/is-subsequence.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
+    int dp[101][10001];
     int solver(string&s,string& t,int i,int j){
         if(i==0 or j==0){
             return 0;
         }
+        if(dp[i][j]!=-1)return dp[i][j];
         if(s[i-1]==t[j-1]){
-            return 1+solver(s,t,i-1,j-1);
+            return dp[i][j]=1+solver(s,t,i-1,j-1);
         }
         else{
-            return solver(s,t,i,j-1);
+            return dp[i][j]=solver(s,t,i,j-1);
         }
     }
     bool isSubsequence(string s, string t) {
         int m=s.size();
         int n=t.size();
+        memset(dp,-1,sizeof dp);
         if(m>n){
             return false;
         }
