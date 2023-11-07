b91617a007415feeb230036d19eb3ae9b56170b4
Perceiver13 <63909437+Sundaran13@users.noreply.github.com>
2023-11-07 16:09:58 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (57.21%) - LeetSync

--- 140-word-break-ii/word-break-ii.cpp
+++ 140-word-break-ii/word-break-ii.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
     vector<string> ans;
+    unordered_set<string> st;
     void solver(string &s,unordered_set<string>&st,string curr,int i){
         if(i==s.length()){
             curr.pop_back();
             ans.push_back(curr);
+            st.insert(curr);
         }
+        if(st.count(curr))return;
         string res="";
         for(int j=i;j<s.length();j++){
             res.push_back(s[j]);
