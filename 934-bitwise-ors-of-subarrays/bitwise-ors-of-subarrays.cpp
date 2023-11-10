b75eb0282df87654db549659913e8c6c95d2599c
Perceiver13 <63909437+Sundaran13@users.noreply.github.com>
2023-11-10 15:29:50 +0530

    Time: 415 ms (77.59%) | Memory: 116 MB (81.28%) - LeetSync

--- 934-bitwise-ors-of-subarrays/bitwise-ors-of-subarrays.cpp
+++ 934-bitwise-ors-of-subarrays/bitwise-ors-of-subarrays.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
-    int subarrayBitwiseORs(vector<int>& arr) {
-        unordered_set<int> s;
-        for(int i=0;i<arr.size();i++){
-            int j=i-1;
-            int x=0,y=arr[i];
-            s.insert(y);
-            while(j>=0 and x!=y){
-                x|=arr[j];
-                y|=arr[j];
-                s.insert(y);
-                j--;
-            }
+    int maxi=0;
+    unordered_set<int> st;
+    void solver(vector<int>&arr,int i){
+        if(i>=arr.size()){
+            return ;
         }
-        return s.size();
+        int j=i-1;
+        int x=0,y=arr[i];
+        st.insert(y);
+        while(j>=0 and x!=y){
+            x|=arr[j];
+            y|=arr[j];
+            st.insert(y);
+            j--;
+        }
+        solver(arr,i+1);
+       
+    }
+    int subarrayBitwiseORs(vector<int>& arr) {
+        solver(arr,0);
+        return st.size();
     }
 };
