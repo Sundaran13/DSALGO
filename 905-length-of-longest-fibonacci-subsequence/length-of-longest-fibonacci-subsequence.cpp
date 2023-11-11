6ef7b58fb31b2215eb5073fde36bb662d2efa0c7
Perceiver13 <63909437+Sundaran13@users.noreply.github.com>
2023-11-11 14:11:09 +0530

    Time: 375 ms (49.77%) | Memory: 12.6 MB (67.36%) - LeetSync

---
+++ 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int solver(vector<int>&arr,unordered_map<int,int>&mp,int i,int j){
+        if(mp.find(arr[i]+arr[j])==mp.end()){
+            return 0;
+        }
+        return 1+solver(arr,mp,j,mp[arr[i]+arr[j]]);
+    }
+    int lenLongestFibSubseq(vector<int>& arr) {
+        unordered_map<int,int> mp;
+        int n=arr.size();
+        for(int i=0;i<n;i++){
+            mp[arr[i]]=i;
+        }
+        int mAx=0;
+        for(int i=0;i<n-2;i++){
+            for(int j=i+1;j<n-1;j++){
+                if(mp.find(arr[i]+arr[j])==mp.end()){
+                    continue;
+                }
+                mAx=max(mAx,solver(arr,mp,i,j)+2);
+            }
+        }
+        return mAx;
+    }
+};
