3ef592cb9d9ac84801a16a430b01ff69ad4bce99
Perceiver13 <63909437+Sundaran13@users.noreply.github.com>
2023-11-12 22:21:11 +0530

    Time: 4 ms (63.89%) | Memory: 6.4 MB (87.93%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.cpp
+++ 10-regular-expression-matching/regular-expression-matching.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    int dp[21][21];
     bool solver(string &s, string &p,int i,int j){
         if(i==-1 and j==-1){
             return true;
@@ -11,6 +12,7 @@ public:
             if(j>=0 and p[j]=='*')return solver(s,p,i,j-2);
             else return false;
         }
+        if(dp[i][j]!=-1)return dp[i][j];
         int ch1=0,ch2=0;
         if(p[j]>='a' and p[j]<='z'){
             if(s[i]==p[j]){
@@ -23,6 +25,7 @@ public:
         else{
             char prev=p[j-1];
             if(prev=='.'){
+                //move behind in string s ,so as to getmore characters being equal to dots in p . 
                 ch1=solver(s,p,i-1,j);
                 ch2=solver(s,p,i,j-2);
             }
@@ -33,9 +36,10 @@ public:
                 ch2=solver(s,p,i,j-2);
             }
         }
-        return ch1 or ch2;
+        return dp[i][j]=ch1 or ch2;
     }
     bool isMatch(string s, string p) {
+        memset(dp,-1,sizeof dp);
         return solver(s,p,s.size()-1,p.size()-1);
     }
 };
