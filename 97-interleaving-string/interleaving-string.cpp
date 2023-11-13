990c5c93948c1f47298f52eccec72b729846282c
Perceiver13 <63909437+Sundaran13@users.noreply.github.com>
2023-11-13 12:38:37 +0530

    Time: 2316 ms (5.01%) | Memory: 7.4 MB (50.04%) - LeetSync

--- 97-interleaving-string/interleaving-string.cpp
+++ 97-interleaving-string/interleaving-string.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
-    bool solver(string &s1,string &s2,string &s3,int i,int j,int k){
+    
+    bool solver(string &s1,string &s2,string &s3,int i,int j,int k,vector<vector<int>>&dp){
         if(i<0 and j<0 and k<0){
             return true;
         }
+        if(i>=0 and j>=0 and dp[i][j]!=-1)return dp[i][j];
         if(i>=0 and s1[i]==s3[k] and j>=0 and s2[j]==s3[k]){
-            return solver(s1,s2,s3,i-1,j,k-1) or solver(s1,s2,s3,i,j-1,k-1);
+            return solver(s1,s2,s3,i-1,j,k-1,dp) or solver(s1,s2,s3,i,j-1,k-1,dp);
         }
         else if(i>=0 and s1[i]==s3[k]){
-            return solver(s1,s2,s3,i-1,j,k-1);
+            return solver(s1,s2,s3,i-1,j,k-1,dp);
         }
         else if(j>=0 and s2[j]==s3[k]){
-            return solver(s1,s2,s3,i,j-1,k-1);
+            return solver(s1,s2,s3,i,j-1,k-1,dp);
         }
         else{
             return false;
@@ -19,9 +21,10 @@ public:
     }
     bool isInterleave(string s1, string s2, string s3) {
         int l=s1.size(),m=s2.size(),n=s3.size();
+        vector<vector<int>>dp(l+1,vector<int>(m+1,-1));
         if(n!=l+m){
             return false;
         }
-        return solver(s1,s2,s3,l-1,m-1,n-1);
+        return solver(s1,s2,s3,l-1,m-1,n-1,dp);
     }
 };
