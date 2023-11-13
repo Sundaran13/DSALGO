a72902016dba6cfb3abd9991af1663bbcf40d7fd
Perceiver13 <63909437+Sundaran13@users.noreply.github.com>
2023-11-13 14:33:08 +0530

    Time: 56 ms (35.17%) | Memory: 24.8 MB (38.67%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,23 +1,39 @@
 class Solution {
 public:
-    int maxi=0;
-    int dp[1001][1001];
-    int solver(string &text1,string &text2,int i,int j){
-        if(i>=text1.size() or j>=text2.size()){
-            return 0;
-        }
-        if(dp[i][j]!=-1)return dp[i][j];
-        ///i//nt ans=0;
-        if(text1[i]==text2[j]){
-            return dp[i][j]=1+solver(text1,text2,i+1,j+1);
-        }        
-        else{
-            return dp[i][j]=max(solver(text1,text2,i+1,j),solver(text1,text2,i,j+1));
-        }
-    }
+    // int maxi=0;
+    // //int dp[1001][1001];
+    // int solver(string &text1,string &text2,int i,int j){
+    //     if(i>=text1.size() or j>=text2.size()){
+               
+    //         return 0;
+    //     }
+
+    //     if(dp[i][j]!=-1)return dp[i][j];
+    //     ///i//nt ans=0;
+    //     //string ans="";
+    //     if(text1[i]==text2[j]){
+    //         return 1+solver(text1,text2,i+1,j+1);
+    //     }        
+    //     else{
+    //         return dp[i][j]=max(solver(text1,text2,i+1,j),solver(text1,text2,i,j+1));
+    //     }
+    // }
     int longestCommonSubsequence(string text1, string text2) {
-        memset(dp,-1,sizeof dp);
-        return solver(text1,text2,0,0);
+        //memset(dp,-1,sizeof dp);
+        vector<vector<int>>dp(text1.size()+1,vector<int>(text2.size()+1,0));
+        for(int i=0;i<=text1.size();i++)dp[i][0]=0;
+        for(int j=0;j<=text2.size();j++)dp[0][j]=0;
+        for(int i=1;i<=text1.size();i++){
+            for(int j=1;j<=text2.size();j++){
+                    if(text1[i-1]==text2[j-1]){
+                        dp[i][j]=1+dp[i-1][j-1];
+                    }        
+                    else{
+                        dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                    }
+                }
+            }
+        return dp[text1.size()][text2.size()];
         //return maxi;
     }
 };
