75d35aa5a47426106f8188c1a9423c31e63b0419
Perceiver13 <63909437+Sundaran13@users.noreply.github.com>
2023-11-13 22:45:17 +0530

    Time: 1277 ms (5.02%) | Memory: 24.8 MB (56.17%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public: 
-   // int dp[4004][4004];
-    bool solver(string &s,string&p,int i,int j,vector<vector<int>>&dp){
+   int dp[2001][2001];
+    bool solver(string &s,string&p,int i,int j){
         if(i<0 and j<0){
             return true;
         }
@@ -18,18 +18,18 @@ public:
         }
          if(dp[i][j]!=-1)return dp[i][j];
         if(s[i]==p[j] or p[j]=='?'){
-            return dp[i][j]=solver(s,p,i-1,j-1,dp);
+            return dp[i][j]=solver(s,p,i-1,j-1);
         }
         else if(p[j]=='*'){
-            return dp[i][j]=solver(s,p,i-1,j,dp) or solver(s,p,i,j-1,dp);
+            return dp[i][j]=solver(s,p,i-1,j) or solver(s,p,i,j-1);
         }
         else{
-            return false;
+            return dp[i][j]=false;
         }
     }
     bool isMatch(string s, string p) {
-        vector<vector<int>> dp(s.length(),vector<int>(p.length(),-1));
-        //memset(dp,-1,sizeof dp);
-        return solver(s,p,s.size()-1,p.size()-1,dp);
+        //vector<vector<int>> dp(s.length(),vector<int>(p.length(),-1));
+        memset(dp,-1,sizeof dp);
+        return solver(s,p,s.size()-1,p.size()-1);
     }
 };
