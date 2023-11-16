bfd447a3b1fa7d0db8943f1807ca8e2eab78ab34
Perceiver13 <63909437+Sundaran13@users.noreply.github.com>
2023-11-16 22:14:47 +0530

    Time: 24 ms (13.86%) | Memory: 13.6 MB (37.48%) - LeetSync

--- 1324-where-will-the-ball-fall/where-will-the-ball-fall.cpp
+++ 1324-where-will-the-ball-fall/where-will-the-ball-fall.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
+    int dp[101][101];
     int solver(vector<vector<int>>&grid,int col,int row){
         if(row>=grid.size()){
             return col;
         }
+        if(dp[row][col]!=-1)return dp[row][col];
         if(grid[row][col]==1){
-            if(col+1>=grid[0].size() or grid[row][col+1]==-1)return -1;
-            return solver(grid,col+1,row+1);
+            if(col+1>=grid[0].size() or grid[row][col+1]==-1)return dp[row][col]= -1;
+            return dp[row][col]=solver(grid,col+1,row+1);
         }
         else{
-            if(col-1<0 or grid[row][col-1]==1)return -1;
-            return solver(grid,col-1,row+1);
+            if(col-1<0 or grid[row][col-1]==1)return dp[row][col]= -1;
+            return dp[row][col]=solver(grid,col-1,row+1);
         }
         
     }
     vector<int> findBall(vector<vector<int>>& grid) {
         int n=grid[0].size();
         vector<int> vec(grid[0].size());
+        memset(dp,-1,sizeof dp);
         for(int i=0;i<n;i++){
             vec[i]=solver(grid,i,0);
         }
