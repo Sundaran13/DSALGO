cf7af523bb033a7d60b82b7cb3e33e9d9db3323b
Perceiver13 <63909437+Sundaran13@users.noreply.github.com>
2023-11-22 19:44:34 +0530

    Time: 66 ms (70.96%) | Memory: 19.3 MB (86.74%) - LeetSync

--- 221-maximal-square/maximal-square.cpp
+++ 221-maximal-square/maximal-square.cpp
@@ -10,11 +10,22 @@ public:
     }
     int maximalSquare(vector<vector<char>>& matrix) {
         int ans=0;
-        memset(dp,-1,sizeof dp);
-        for(int i=0;i<matrix.size();i++){
-            for(int j=0;j<matrix[0].size();j++){
-                
-                ans=max(ans,solver(matrix,i,j));
+        memset(dp,0,sizeof dp);
+        for(int i=matrix.size()-1;i>=0;i--){
+            for(int j=matrix[0].size()-1;j>=0;j--){
+                if(j==matrix[0].size()-1){
+                    dp[i][j]=matrix[i][j]=='0'? 0 : 1;
+                }
+                else if(i==matrix.size()-1){
+                    dp[i][j]=matrix[i][j]=='0'? 0 : 1;
+                }
+                else if(matrix[i][j]=='0'){
+                    dp[i][j]=0;
+                }
+                else{
+                    dp[i][j]=min({dp[i+1][j],dp[i][j+1],dp[i+1][j+1]})+1;
+                }
+                ans=max(ans,dp[i][j]);                
             }
         }
         return ans*ans;
